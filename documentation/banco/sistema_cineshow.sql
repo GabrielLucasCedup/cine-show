acf49a723227fd7c52419ff77ce812106150f3fc
LucasLessaAnacleto <lucaslessaanacleto@gmail.com>
2023-06-26 20:18:54 -0300

    export banco de dados

--- documentation/banco/sistema_cineshow.sql
+++ documentation/banco/sistema_cineshow.sql
@@ -1,85 +1,140 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `sistema_cineshow` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sistema_cineshow`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Tempo de geração: 31-Maio-2023 às 14:50
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 8.0.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: sistema_cineshow
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `sistema_cineshow`
+-- Table structure for table `favoritos`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `favoritos`
---
-
+DROP TABLE IF EXISTS `favoritos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `favoritos` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `usuario_id` varchar(6) DEFAULT NULL,
-  `midia_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `midia_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `midia_id` (`midia_id`),
+  CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`midia_id`) REFERENCES `midia` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `generos`
+-- Dumping data for table `favoritos`
 --
 
+LOCK TABLES `favoritos` WRITE;
+/*!40000 ALTER TABLE `favoritos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favoritos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `generos`
+--
+
+DROP TABLE IF EXISTS `generos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `generos` (
-  `id` int(11) NOT NULL,
-  `nome_genero` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_genero` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `midia`
+-- Dumping data for table `generos`
 --
 
+LOCK TABLES `generos` WRITE;
+/*!40000 ALTER TABLE `generos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `generos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `midia`
+--
+
+DROP TABLE IF EXISTS `midia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `midia` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome_midia` varchar(100) NOT NULL,
-  `dir_midia` text DEFAULT NULL,
-  `dir_capa` text DEFAULT NULL,
-  `duracao` int(11) NOT NULL,
+  `dir_midia` text,
+  `dir_capa` text,
+  `duracao` int NOT NULL,
   `data_lancamento` date NOT NULL,
-  `genero_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `genero_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `genero_id` (`genero_id`),
+  CONSTRAINT `midia_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `generos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `retomar`
+-- Dumping data for table `midia`
 --
 
+LOCK TABLES `midia` WRITE;
+/*!40000 ALTER TABLE `midia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `midia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `retomar`
+--
+
+DROP TABLE IF EXISTS `retomar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `retomar` (
-  `id` int(11) NOT NULL,
-  `tempo_atual` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tempo_atual` int NOT NULL,
   `ultimo_acesso` date NOT NULL,
   `usuario_id` varchar(6) DEFAULT NULL,
-  `midia_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `midia_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `midia_id` (`midia_id`),
+  CONSTRAINT `retomar_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `retomar_ibfk_2` FOREIGN KEY (`midia_id`) REFERENCES `midia` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `usuario`
+-- Dumping data for table `retomar`
 --
 
+LOCK TABLES `retomar` WRITE;
+/*!40000 ALTER TABLE `retomar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `retomar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id` varchar(6) NOT NULL,
   `nome` varchar(100) NOT NULL,
@@ -87,102 +142,28 @@ CREATE TABLE `usuario` (
   `email` varchar(255) NOT NULL,
   `senha` varchar(16) NOT NULL,
   `data_nasc` date NOT NULL,
-  `data_registro` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `data_registro` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `usuario`
 --
 
---
--- Índices para tabela `favoritos`
---
-ALTER TABLE `favoritos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario_id` (`usuario_id`),
-  ADD KEY `midia_id` (`midia_id`);
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tabela `generos`
---
-ALTER TABLE `generos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `midia`
---
-ALTER TABLE `midia`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `genero_id` (`genero_id`);
-
---
--- Índices para tabela `retomar`
---
-ALTER TABLE `retomar`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario_id` (`usuario_id`),
-  ADD KEY `midia_id` (`midia_id`);
-
---
--- Índices para tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `favoritos`
---
-ALTER TABLE `favoritos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `generos`
---
-ALTER TABLE `generos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `midia`
---
-ALTER TABLE `midia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `retomar`
---
-ALTER TABLE `retomar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `favoritos`
---
-ALTER TABLE `favoritos`
-  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
-  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`midia_id`) REFERENCES `midia` (`id`);
-
---
--- Limitadores para a tabela `midia`
---
-ALTER TABLE `midia`
-  ADD CONSTRAINT `midia_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `generos` (`id`);
-
---
--- Limitadores para a tabela `retomar`
---
-ALTER TABLE `retomar`
-  ADD CONSTRAINT `retomar_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
-  ADD CONSTRAINT `retomar_ibfk_2` FOREIGN KEY (`midia_id`) REFERENCES `midia` (`id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-26 20:17:20
